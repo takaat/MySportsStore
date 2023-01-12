b303ad80fa8dc3d6428649a1f1c34c04515c1ac2
mana.u <great8life@gmail.com>
2023-01-12 22:05:58 +0900

    Initial Commit

---
+++ MySportsStore/MySportsStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySportsStoreApp.swift
+//  MySportsStore
+//
+//  Created by mana on 2023/01/12.
+//
+
+import SwiftUI
+
+@main
+struct MySportsStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
