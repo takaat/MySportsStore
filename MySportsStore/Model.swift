39aac6c2f21d5c28ef108d673fc14d45e2e6a89d
mana.u <great8life@gmail.com>
2023-01-18 22:02:28 +0900

    チャプター４

---
+++ MySportsStore/Model.swift
@@ -0,0 +1,40 @@
+//
+//  Model.swift
+//  MySportsStore
+//
+//  Created by mana on 2023/01/18.
+//
+
+import Foundation
+
+struct Product {
+    private(set) var name:String;
+    private(set) var description:String;
+    private(set) var category:String;
+    private var stockLevelBackingValue:Int = 0;
+    private var priceBackingValue:Double = 0;
+
+    init(name:String, description:String, category:String, price:Double, stockLevel:Int) {
+        self.name = name;
+        self.description = description;
+        self.category = category;
+        self.price = price;
+        self.stockLevel = stockLevel;
+    }
+
+    var stockLevel:Int {
+        get { return stockLevelBackingValue; }
+        set { stockLevelBackingValue = max(0, newValue); }
+    }
+
+    private(set) var price:Double {
+        get { return priceBackingValue; }
+        set { priceBackingValue = max(1, newValue); }
+    }
+
+    var stockValue:Double {
+        get {
+            return price * Double(stockLevel);
+        }
+    }
+}
